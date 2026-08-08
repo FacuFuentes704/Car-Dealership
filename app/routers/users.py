a1987a7d1041487c38e2b14f586be7eb5c863087
Facundo Fuentes <facufuentes704@gmail.com>
2026-08-08 19:51:32 -0300

    feat: add all routers and protect register endpoint

--- app/routers/users.py
+++ app/routers/users.py
@@ -10,8 +10,8 @@ users_router = APIRouter(prefix="/users", tags=["users"])
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @users_router.post("/register", response_model=UserResponse)
-def register(user_data: UserCreate, db: Session = Depends(get_db)):
-    return register_user(db, user_data)
+def register(user_data: UserCreate, db: Session = Depends(get_db), user_id: User = Depends(get_current_user)):
+    return register_user(user_data, db)
 
 @auth_router.post("/login", response_model=TokenResponse)
 def login(user_data: UserLogin, db: Session = Depends(get_db)):
